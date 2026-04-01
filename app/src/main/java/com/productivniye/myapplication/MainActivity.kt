6b3127eeb6583582df7ded440cf21d68df546814
Raito <raimutsuki2811@gmail.com>
2026-04-02 00:16:07 +0300

    Plus button

--- app/src/main/java/com/productivniye/myapplication/MainActivity.kt
+++ app/src/main/java/com/productivniye/myapplication/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.productivniye.myapplication
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,21 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Находим элементы по их id
-        val editTextName = findViewById<EditText>(R.id.editTextName)
-        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
-        val resultText = findViewById<TextView>(R.id.resultText)
+        val plusBut = findViewById<ImageView>(R.id.plusBut)
 
-        // Обработка нажатия кнопки
-        buttonSubmit.setOnClickListener {
-            val name = editTextName.text.toString().trim()
-            if (name.isNotEmpty()) {
-                val message = "Привет, $name! Рад тебя видеть!"
-                resultText.text = message
-                editTextName.text.clear()
-            } else {
-                Toast.makeText(this, "Пожалуйста, введите имя!", Toast.LENGTH_SHORT).show()
-            }
+        plusBut.setOnClickListener {
+            Toast.makeText(this, "Плюсик нажат!", Toast.LENGTH_SHORT).show()
         }
     }
 }
